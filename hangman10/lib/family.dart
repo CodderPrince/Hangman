4d5b9048eae9f7bef756a3159314a10d5debe608
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 00:10:30 +0600

    Create family.dart

---
+++ hangman10/lib/family.dart
@@ -0,0 +1,21 @@
+class FamilyData {
+  static Map<String, List<String>> categoryWordBank = {
+    "Family": [
+      "PRINCE",
+      "NAZMUL",
+      "NISHAT",
+      "JAHANARA",
+      "AMIRUL",
+      "PRIYANKA",
+    ],
+  };
+
+  static Map<String, String> imageMapping = {
+    "PRINCE": "assets/images/PRINCE.jpg",
+    "NAZMUL": "assets/images/NAZMUL.jpg",
+    "NISHAT": "assets/images/NISHAT.jpg",
+    "JAHANARA": "assets/images/JAHANARA.jpg",
+    "AMIRUL": "assets/images/AMIRUL.jpg",
+    "PRIYANKA": "assets/images/PRIYANKA.jpg",
+  };
+}

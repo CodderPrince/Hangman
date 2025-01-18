4a1def6ceed4c5c44a0aa32050715a631cc6b6ad
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 00:25:42 +0600

    Create animal.dart

---
+++ hangman17/lib/animal.dart
@@ -0,0 +1,51 @@
+class AnimalData {
+  static Map<String, List<String>> categoryWordBank = {
+    "Animal": [
+      "BAT", "BEAR", "BISON", "BUFFALO", "CAMEL", "CAT", "COW", "DEER",
+      "DOG", "DONKEY", "ELEPHANT", "FOX", "FROG", "GIRAFFE", "GOAT", "HARE",
+      "HORSE", "HYENA", "JAGUAR", "KANGAROO", "KOALA", "LION", "LLAMA",
+      "MOLE", "MONKEY", "MULE", "ORANGUTAN", "OTTER", "PANDA", "PIG", "RABBIT",
+      "RAT", "SEAL", "SHEEP", "TIGER", "WOLF", "ZEBRA"
+    ],
+  };
+
+  static Map<String, String> imageMapping = {
+    "BAT": "assets/images/animal/BAT.jpg",
+    "BEAR": "assets/images/animal/BEAR.jpg",
+    "BISON": "assets/images/animal/BISON.jpg",
+    "BUFFALO": "assets/images/animal/BUFFALO.jpg",
+    "CAMEL": "assets/images/animal/CAMEL.jpg",
+    "CAT": "assets/images/animal/CAT.jpg",
+    "COW": "assets/images/animal/COW.jpg",
+    "DEER": "assets/images/animal/DEER.jpg",
+    "DOG": "assets/images/animal/DOG.jpg",
+    "DONKEY": "assets/images/animal/DONKEY.jpg",
+    "ELEPHANT": "assets/images/animal/ELEPHANT.jpg",
+    "FOX": "assets/images/animal/FOX.jpg",
+    "FROG": "assets/images/animal/FROG.jpg",
+    "GIRAFFE": "assets/images/animal/GIRAFFE.jpg",
+    "GOAT": "assets/images/animal/GOAT.jpg",
+    "HARE": "assets/images/animal/HARE.jpg",
+    "HORSE": "assets/images/animal/HORSE.jpg",
+    "HYENA": "assets/images/animal/HYENA.jpg",
+    "JAGUAR": "assets/images/animal/JAGUAR.jpg",
+    "KANGAROO": "assets/images/animal/KANGAROO.jpg",
+    "KOALA": "assets/images/animal/KOALA.jpg",
+    "LION": "assets/images/animal/LION.jpg",
+    "LLAMA": "assets/images/animal/LLAMA.jpg",
+    "MOLE": "assets/images/animal/MOLE.jpg",
+    "MONKEY": "assets/images/animal/MONKEY.jpg",
+    "MULE": "assets/images/animal/MULE.jpg",
+    "ORANGUTAN": "assets/images/animal/ORANGUTAN.jpg",
+    "OTTER": "assets/images/animal/OTTER.jpg",
+    "PANDA": "assets/images/animal/PANDA.jpg",
+    "PIG": "assets/images/animal/PIG.jpg",
+    "RABBIT": "assets/images/animal/RABBIT.jpg",
+    "RAT": "assets/images/animal/RAT.jpg",
+    "SEAL": "assets/images/animal/SEAL.jpg",
+    "SHEEP": "assets/images/animal/SHEEP.jpg",
+    "TIGER": "assets/images/animal/TIGER.jpg",
+    "WOLF": "assets/images/animal/WOLF.jpg",
+    "ZEBRA": "assets/images/animal/ZEBRA.jpg"
+  };
+}

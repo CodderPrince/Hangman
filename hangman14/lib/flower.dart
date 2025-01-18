a2eb8c6d846e23a9ec96c36c6153a141b98f9a7b
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 00:19:44 +0600

    Create flower.dart

---
+++ hangman14/lib/flower.dart
@@ -0,0 +1,9 @@
+class FlowerData {
+  static Map<String, List<String>> categoryWordBank = {
+    "Flower": ["JOBA","RED"],
+  };
+  static Map<String, String> imageMapping = {
+    "JOBA": "assets/images/flower/JOBA.jpg",
+    "RED": "assets/images/flower/RED.jpg",
+  };
+}

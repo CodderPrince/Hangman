b1fdd14f3497b8e1434310a17b10d9ab666308f7
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:14:03 +0600

    Create image_mapping.dart

---
+++ hangman8/lib/data/image_mapping.dart
@@ -0,0 +1,13 @@
+final Map<String, String> imageMapping = {
+  // Family category images
+  "PRINCE": "assets/images/family/PRINCE.jpg",
+  "NAZMUL": "assets/images/family/NAZMUL.jpg",
+  "NISHAT": "assets/images/family/NISHAT.jpg",
+  "JAHANARA": "assets/images/family/JAHANARA.jpg",
+  "AMIRUL": "assets/images/family/AMIRUL.jpg",
+  "PRIYANKA": "assets/images/family/PRIYANKA.jpg",
+
+  // Cousin category images
+  "SH": "assets/images/cousin/SH.jpg",
+  "NOO": "assets/images/cousin/NOO.jpg",
+};
